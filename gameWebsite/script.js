10f5961bbebd3c26e6e50decb2dead2cdc1ebe58
Ryan Marr <123612869+RyanMarr1@users.noreply.github.com>
2024-04-24 17:47:40 -0700

    Add files via upload

---
+++ gameWebsite/script.js
@@ -0,0 +1,7 @@
+
+<script>
+  document.getElementById("coinflip").addEventListener("click", function() {
+    // Redirect to another page
+    window.location.href = "games/coinflip.html";
+  });
+</script>
