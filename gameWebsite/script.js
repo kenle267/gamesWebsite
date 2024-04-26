152222b3120ad1b61337e3e86b6fddb22a113630
Kenny Le <kennyle9001@gmail.com>
2024-04-25 18:35:32 -0700

    testing push and pull

--- gameWebsite/script.js
+++
@@ -1,7 +0,0 @@
-
-<script>
-  document.getElementById("coinflip").addEventListener("click", function() {
-    // Redirect to another page
-    window.location.href = "games/coinflip.html";
-  });
-</script>
