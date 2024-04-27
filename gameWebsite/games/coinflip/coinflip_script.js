ce228104622035159d103d4c61574186949ee9bd
RyanMarr1 <ramarr@ucdavis.edu>
2024-04-26 20:53:16 -0700

    Organized games into respective folders

---
+++ gameWebsite/games/coinflip/coinflip_script.js
@@ -0,0 +1,30 @@
+const result = document.querySelector('.result'); 
+const coinIcon = document.getElementById('coin'); 
+const button =  document.getElementById('btn'); 
+coinIcon.insertAdjacentElement('afterend', result); // places result right after coin image and before flip button
+
+document.getElementById("btn").addEventListener("click", function() {
+  let side; // the side the coin lands on
+  let imageUrl; // picture of the side
+  const randomVal = Math.random(); // random val between [0,1]
+  if (randomVal < 0.5) {
+    side = 'Heads';
+  } else {
+    side = 'Tails';
+  }
+  imageUrl = side === 'Heads' ? 
+  'https://media.geeksforgeeks.org/wp-content/uploads/20231016151817/heads.png' : 
+  'https://media.geeksforgeeks.org/wp-content/uploads/20231016151806/tails.png'; 
+  
+  coinIcon.classList.add('flip'); 
+  setTimeout(updateHTMl = () => { // arrow function to update html with coin image and add and remove .flip class
+    coinIcon.innerHTML = `<img src="${imageUrl}" alt="${side}">`; // changes the coin's image in html file
+    coinIcon.classList.remove('flip');  
+    setTimeout(updateResult = () => { // arrow function to update .result class with side coin landed on
+      result.textContent = `You flipped: ${side}`; 
+      result.style.opacity = 1; 
+      button.disabled = false; 
+    }, 200); 
+  }, 1000); 
+});
+
